a35e799e8ced7412c1e0b9adc43f24d912c252eb
anupo <anupomr@yahoo.com>
2020-05-06 08:22:50 -0400

    Data is prepaired for test

--- ML_Regrassion.R
+++ ML_Regrassion.R
@@ -1,1 +1,17 @@
 # Multiple Linear Regrassion 
+
+# Importing the dataset
+dataset = read.csv('50_Startups.csv')
+
+# Encoding categorical data
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1, 2, 3))
+
+# Spliting the dataset into training set and test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
