7ecd2f1be89d07b946c04df1435d4c0e017172b5
anupo <anupomr@yahoo.com>
2020-05-09 19:38:30 -0400

    Fitting Linear Regression to the dataset

---
+++ PolynomialRegression.R
@@ -0,0 +1,11 @@
+# Polynomial Regression 
+
+# Imporing the dataset
+dataset =  read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Spliting the dataset into traing set and Test set
+# install.packages('caTools')
+library(caTools)
+
+
