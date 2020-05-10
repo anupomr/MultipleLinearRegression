5f333a31328a5f98fb4a68217236ec7a994d5add
anupo <anupomr@yahoo.com>
2020-05-10 08:52:58 -0400

    Visualising the polynomial Regression result

--- PolynomialRegression.R
+++ PolynomialRegression.R
@@ -11,6 +11,31 @@ lin_reg =lm(formula = Salary ~ .,
 # Fitting polynomial Regression to the dataset
 dataset$Level2= dataset$Level^2
 dataset$Level3= dataset$Level^3
-lin_reg =lm(formula = Salary ~ .,
+dataset$Level4= dataset$Level^4
+poly_reg =lm(formula = Salary ~ .,
             data = dataset)
 # in console summary(lin_reg)
+
+
+
+# Visualising the Linear Regression result
+#install.packages('ggplot2')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Linear Regression') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Visualising the polynomial Regression result
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Polynomial Regression') +
+  xlab('Level') +
+  ylab('Salary')
