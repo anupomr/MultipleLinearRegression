e0a076fe1f73fff39c117cbba2609b0a7a444c22
anupo <anupomr@yahoo.com>
2020-05-14 18:28:50 -0400

    Visualising the Random Forest Regression model

--- Random_Forest_Regression.R
+++ Random_Forest_Regression.R
@@ -11,8 +11,21 @@ library(randomForest)
 set.seed(1234)
 regressor = randomForest(x = dataset[1],
                          y = dataset$Salary,
-                         ntree = 10)
+                         ntree = 100)
 
 # Predicting a new result
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
+
+# Visualising the Random Forest Regression model (High resoletion )
+#install.packages('ggplot2')
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            colour = 'blue') +
+  ggtitle('Random Forest Regression') +
+  xlab('Level') +
+  ylab('Salary')
